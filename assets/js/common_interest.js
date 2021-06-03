cf5a4623326b31f5ff02b227cc3d42eca05322d7
Justin George <justin.v.george@gmail.com>
2021-06-03 15:03:18 +0400

    made changes to the Common Interest page and fixed the button problem in common interest and give back page

--- assets/js/common_interest.js
+++ assets/js/common_interest.js
@@ -1,65 +1,59 @@
-$(document).ready(function() {
-  $("button a").click(function(e) {
-      e.preventDefault();
+$(document).ready(function () {
+  $('button a').click(function (e) {
+    e.preventDefault();
   });
 });
 
-function displayOptions() 
-  {
-    const searchOptions=document.getElementsByClassName("searchOptions")[0];
-    const mainBody=document.getElementById("main");
-    searchOptions.style.opacity="1"
-    searchOptions.style.transition="all 0.3s"
-    searchOptions.style.pointerEvents="all"
-    mainBody.style.opacity="0";
-    mainBody.style.transition="0.3s"   
-  }
-  function hideOptions() 
-  {
-    const searchOptions=document.getElementsByClassName("searchOptions")[0];
-    const mainBody=document.getElementById("main");
-    searchOptions.style.opacity = "0";
-    searchOptions.style.pointerEvents="none"
-    mainBody.style.opacity="1";
-    mainBody.style.pointerEvents="all"
-  }
-  
-  function displayDirectory()
-  {
-    const hostBody=document.getElementById("host-an-event");
-    const directoryBody=document.getElementById("directorytable");
-    console.log('what');
-     directoryBody.style.display="block"
-     directoryBody.style.transition="all 0.3s"
-     directoryBody.style.pointerEvents="all"
-     mainBody.style.opacity="0";
-     mainBody.style.transition="0.3s"
-  }
-  
-  
-  function clickOnX() 
-  {
-   const hostBody=document.getElementById("host-an-event");
-   const directoryBody=document.getElementById("directorytable");
-   console.log('what');
-   hostBody.style.display="none"
-   hostBody.style.transition="all 0.3s"
-   hostBody.style.pointerEvents="none"
-   directoryBody.style.display="none"
-   directoryBody.style.transition="all 0.3s"
-   directoryBody.style.pointerEvents="none"
-   mainBody.style.opacity="1";
-   mainBody.style.transition="0.3s"
-  }
-  
-  function displayHost()
-  {
-    const hostBody=document.getElementById("host-an-event");
-    const directoryBody=document.getElementById("directorytable");
-    console.log('what');
-     hostBody.style.display="block"
-     hostBody.style.transition="all 0.3s"
-     hostBody.style.pointerEvents="all"
-     mainBody.style.opacity="0";
-     mainBody.style.transition="0.3s"
-  }
+function displayOptions() {
+  const searchOptions = document.getElementsByClassName('searchOptions')[0];
+  const mainBody = document.getElementById('main');
+  searchOptions.style.opacity = '1';
+  searchOptions.style.transition = 'all 0.3s';
+  searchOptions.style.pointerEvents = 'all';
+  mainBody.style.opacity = '0';
+  mainBody.style.transition = '0.3s';
+}
+function hideOptions() {
+  const searchOptions = document.getElementsByClassName('searchOptions')[0];
+  const mainBody = document.getElementById('main');
+  searchOptions.style.opacity = '0';
+  searchOptions.style.pointerEvents = 'none';
+  mainBody.style.opacity = '1';
+  mainBody.style.pointerEvents = 'all';
+}
+
+function displayDirectory() {
+  const hostBody = document.getElementById('host-an-event');
+  const directoryBody = document.getElementById('directorytable');
+  console.log('what');
+  directoryBody.style.display = 'block';
+  directoryBody.style.transition = 'all 0.3s';
+  directoryBody.style.pointerEvents = 'all';
+  mainBody.style.opacity = '0';
+  mainBody.style.transition = '0.3s';
+}
+
+function clickOnX() {
+  const hostBody = document.getElementById('host-an-event');
+  const directoryBody = document.getElementById('directorytable');
+  console.log('what');
+  hostBody.style.display = 'none';
+  hostBody.style.transition = 'all 0.3s';
+  hostBody.style.pointerEvents = 'none';
+  directoryBody.style.display = 'none';
+  directoryBody.style.transition = 'all 0.3s';
+  directoryBody.style.pointerEvents = 'none';
+  mainBody.style.opacity = '1';
+  mainBody.style.transition = '0.3s';
+}
+
+function displayHost() {
+  const hostBody = document.getElementById('host-an-event');
+  const directoryBody = document.getElementById('directorytable');
+  console.log('what');
+  hostBody.style.display = 'block';
+  hostBody.style.transition = 'all 0.3s';
+  hostBody.style.pointerEvents = 'all';
+  mainBody.style.opacity = '0';
+  mainBody.style.transition = '0.3s';
+}
